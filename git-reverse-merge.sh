22138fe1e68474010e98461cd06e004a2a05f486
Leandro Freitas <freitass@inf.ufsc.br>
2012-02-15 10:43:36 -0200

    Merge from current branch

--- git-reverse-merge.sh
+++ git-reverse-merge.sh
@@ -6,6 +6,17 @@
 # Also, he advises that this should be used only when merging into production
 # branches, and not development branches.
 
+# Get current branch as source.
+source_branch="$(git symbolic-ref HEAD 2>/dev/null)"
+
+# Assert we are not in a detached branch.
+if [ -z $source_branch ]
+then
+  echo "It is not possible to merge from a detached branch"
+  echo "Stopping"
+  exit 2
+fi
+
 # Reading flags.
 force=
 while getopts f name
@@ -21,28 +32,33 @@ shift $(($OPTIND - 1))
 # Assert at least one branch was specified.
 if [ $# -lt 1 ]
 then
-  echo "No branch was specified"
+  echo "No branch specified"
   echo
-  echo "Usage: $0 [-f] branch0 [... branchN]"
+  echo "Usage: $0 [-f] <branch>..."
   exit 2
 fi
 
 for branch in "$@"
 do
-  if ! ( git checkout $branch && git merge --no-ff master ); then
+  if ! ( git checkout $branch && git merge --no-ff $source_branch ); then
     if [ -z $force ]
     then
-      # Exit on the first error
-      exit 1
+      echo "Something went wrong for branch $branch"
+      git checkout $source_branch
+      echo "Stopping"
+      exit 2
     else
-      # If you want to just plow ahead, do something like this:
-      git reset --hard       # make sure there aren't merge conflicts in the tree
-      failed_merges="$failed_merges $branch"  # remember for later
+      # Restoring current branch and remember it for later.
+      git reset --hard
+      failed_merges="$failed_merges $branch"
     fi
   fi
 done
 
-# If you plowed ahead above, print the branches which failed to checkout+merge
+# Checking out source_branch
+git checkout $source_branch
+
+# If you plowed ahead above, print the branches which failed to checkout+merge.
 if [ -n $force -a -n "$failed_merges" ]; then
     echo "Failed merges: $failed_merges"
 fi
